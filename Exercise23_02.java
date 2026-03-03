0f3813e49c1221332361d453d35e9283044424af
MarcusCeradini <mnceradini@my.waketech.edu>
2026-03-03 10:00:04 -0500

    Update Exercise23_02.java

--- Exercise23_02.java
+++ Exercise23_02.java
@@ -27,62 +27,88 @@ import java.util.Comparator;
 public class Exercise23_02 {
 
     // ---------------------------------------------------------------
-    // TODO 1: Implement mergeSort using Comparable
-    //
-    // Method signature:
-    //   public static <E extends Comparable<E>> void mergeSort(E[] list)
-    //
-    // Steps:
-    //   - Base case: if list.length <= 1, return (already sorted)
-    //   - Create firstHalf: (E[]) new Object[list.length / 2]
-    //   - Copy first half from list using System.arraycopy
-    //   - Create secondHalf: (E[]) new Object[list.length - list.length / 2]
-    //   - Copy second half from list using System.arraycopy
-    //   - Recursively call mergeSort on firstHalf and secondHalf
-    //   - Call merge(firstHalf, secondHalf, list)
+    public static <E extends Comparable<E>> void mergeSort(E[] list) {
+        if (list.length > 1) {
+            E[] firstHalf = (E[]) new Comparable[list.length / 2];
+            System.arraycopy(list, 0, firstHalf, 0,
+                    list.length / 2);
+
+            int secondHalfLength = list.length - list.length / 2;
+            E[] secondHalf = (E[]) new Comparable[list.length - list.length / 2];
+            System.arraycopy(list, list.length / 2,
+                    secondHalf, 0, secondHalfLength);
+
+            mergeSort(firstHalf);
+            mergeSort(secondHalf);
+
+            merge(firstHalf, secondHalf, list);
+        }
+    }
     // ---------------------------------------------------------------
 
 
     // ---------------------------------------------------------------
-    // TODO 2: Implement merge helper for Comparable version
-    //
-    // Method signature:
-    //   private static <E extends Comparable<E>>
-    //   void merge(E[] list1, E[] list2, E[] temp)
-    //
-    // Steps:
-    //   - Three pointers: current1=0, current2=0, current3=0
-    //   - While both lists have elements:
-    //       if list1[current1].compareTo(list2[current2]) < 0
-    //           take from list1
-    //       else
-    //           take from list2
-    //   - Copy any remaining elements from list1
-    //   - Copy any remaining elements from list2
+    private static <E extends Comparable<E>> void merge(E[] list1, E[] list2, E[] temp) {
+        int current1 = 0;
+        int current2 = 0;
+        int current3 = 0;
+
+        while (current1 < list1.length && current2 < list2.length) {
+            if (list1[current1].compareTo(list2[current2]) < 0)
+                temp[current3++] = list1[current1++];
+            else
+                temp[current3++] = list2[current2++];
+        }
+
+        while (current1 < list1.length)
+            temp[current3++] = list1[current1++];
+
+        while (current2 < list2.length)
+            temp[current3++] = list2[current2++];
+    }
     // ---------------------------------------------------------------
 
 
     // ---------------------------------------------------------------
-    // TODO 3: Implement mergeSort using Comparator
-    //
-    // Method signature:
-    //   public static <E> void mergeSort(E[] list, Comparator<? super E> comparator)
-    //
-    // Same structure as TODO 1, but:
-    //   - Pass comparator to both recursive mergeSort calls
-    //   - Pass comparator to the merge call
+    public static <E> void mergeSort(E[] list, Comparator<? super E> comparator){
+        if (list.length > 1) {
+            E[] firstHalf = (E[]) new Object[list.length / 2];
+            System.arraycopy(list, 0, firstHalf, 0,
+                    list.length / 2);
+
+            int secondHalfLength = list.length - list.length / 2;
+            E[] secondHalf = (E[]) new Object[secondHalfLength];
+            System.arraycopy(list, list.length / 2,
+                    secondHalf, 0, secondHalfLength);
+
+            mergeSort(firstHalf, comparator);
+            mergeSort(secondHalf, comparator);
+
+            merge(firstHalf, secondHalf, list, comparator);
+        }
+    }
     // ---------------------------------------------------------------
 
 
     // ---------------------------------------------------------------
-    // TODO 4: Implement merge helper for Comparator version
-    //
-    // Method signature:
-    //   private static <E>
-    //   void merge(E[] list1, E[] list2, E[] temp, Comparator<? super E> comparator)
-    //
-    // Same structure as TODO 2, but use:
-    //   comparator.compare(list1[current1], list2[current2]) < 0
+    private static <E> void merge(E[] list1, E[] list2, E[] temp, Comparator<? super E> comparator){
+        int current1 = 0;
+        int current2 = 0;
+        int current3 = 0;
+
+        while (current1 < list1.length && current2 < list2.length) {
+            if (comparator.compare(list1[current1], list2[current2]) < 0)
+                temp[current3++] = list1[current1++];
+            else
+                temp[current3++] = list2[current2++];
+        }
+
+        while (current1 < list1.length)
+            temp[current3++] = list1[current1++];
+
+        while (current2 < list2.length)
+            temp[current3++] = list2[current2++];
+    }
     // ---------------------------------------------------------------
 
 
